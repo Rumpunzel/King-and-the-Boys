1279192fee37f4962cf333c69e23895fe20b9ef6
Jamal Bencharki <bencharki.jamal@gmail.com>
2026-04-18 08:52:13 +0200

    you guessed it, more cleanups

--- engine/game.gd
+++ engine/game.gd
@@ -5,11 +5,8 @@ extends Node
 
 @export_group("Configuration")
 @export var _serializer: Serializer
-@export var _structure_spawner: StructureSpawner
-@export var _thing_spawner: ThingSpawner
-@export var _agent_spawner: AgentSpawner
-@export var _player_ghost_spawner: PlayerGhostSpawner
 @export var _level: Level
+@export var _player_ghost_spawner: PlayerGhostSpawner
 
 func _enter_tree() -> void:
 	if Engine.is_editor_hint(): return
@@ -41,9 +38,6 @@ func load_game() -> Error:
 func stop_game() -> void:
 	print_debug("Stopping running game...")
 	_player_ghost_spawner.stop_synching_players()
-	_agent_spawner.remove_all_agents()
-	_thing_spawner.remove_all_things()
-	_structure_spawner.remove_all_structures()
 	SceneManager.to_main()
 
 func _on_save_requested() -> void:
@@ -58,9 +52,6 @@ func _on_disconnected_from_multiplayer() -> void:
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings: PackedStringArray = []
 	if not _serializer: warnings.append("Missing Serializer reference.")
-	if not _structure_spawner: warnings.append("Missing StructureSpawner reference.")
-	if not _thing_spawner: warnings.append("Missing ThingSpawner reference.")
-	if not _agent_spawner: warnings.append("Missing AgentSpawner reference.")
-	if not _player_ghost_spawner: warnings.append("Missing PlayerGhostSpawner reference.")
 	if not _level: warnings.append("Missing Level reference.")
+	if not _player_ghost_spawner: warnings.append("Missing PlayerGhostSpawner reference.")
 	return warnings
